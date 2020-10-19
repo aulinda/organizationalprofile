c05a96e6f3da17ace427d3bfc97573af14003d68
aulinda buban <buban.139456@cubao.sti.edu.ph>
2020-10-19 23:02:30 +0800

    Init

---
+++ HandsOnActivity2/Form1.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace HandsOnActivity2
+{
+    partial class frmRegistration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStudentNo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.btnRegistration = new System.Windows.Forms.Button();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtMiddleInitial = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.cbPrograms = new System.Windows.Forms.ComboBox();
+            this.datePickerBirthday = new System.Windows.Forms.DateTimePicker();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(48, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student No";
+            // 
+            // txtStudentNo
+            // 
+            this.txtStudentNo.Location = new System.Drawing.Point(135, 70);
+            this.txtStudentNo.Name = "txtStudentNo";
+            this.txtStudentNo.Size = new System.Drawing.Size(134, 20);
+            this.txtStudentNo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(393, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Program";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(48, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(381, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "First Name";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(633, 123);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(19, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "MI";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(48, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Age";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(381, 199);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(42, 13);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Gender";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(48, 253);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Birthday";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(377, 260);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(61, 13);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Contact No";
+            // 
+            // btnRegistration
+            // 
+            this.btnRegistration.Location = new System.Drawing.Point(307, 317);
+            this.btnRegistration.Name = "btnRegistration";
+            this.btnRegistration.Size = new System.Drawing.Size(132, 30);
+            this.btnRegistration.TabIndex = 11;
+            this.btnRegistration.Text = "Register";
+            this.btnRegistration.UseVisualStyleBackColor = true;
+            this.btnRegistration.Click += new System.EventHandler(this.btnRegistration_Click);
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Location = new System.Drawing.Point(135, 127);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(134, 20);
+            this.txtLastName.TabIndex = 12;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(465, 120);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(134, 20);
+            this.txtFirstName.TabIndex = 13;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(135, 192);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(134, 20);
+            this.txtAge.TabIndex = 14;
+            // 
+            // txtMiddleInitial
+            // 
+            this.txtMiddleInitial.Location = new System.Drawing.Point(658, 120);
+            this.txtMiddleInitial.Name = "txtMiddleInitial";
+            this.txtMiddleInitial.Size = new System.Drawing.Size(101, 20);
+            this.txtMiddleInitial.TabIndex = 15;
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Location = new System.Drawing.Point(465, 250);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(134, 20);
+            this.txtContactNo.TabIndex = 18;
+            // 
+            // cbPrograms
+            // 
+            this.cbPrograms.FormattingEnabled = true;
+            this.cbPrograms.Location = new System.Drawing.Point(465, 62);
+            this.cbPrograms.Name = "cbPrograms";
+            this.cbPrograms.Size = new System.Drawing.Size(157, 21);
+            this.cbPrograms.TabIndex = 19;
+            // 
+            // datePickerBirthday
+            // 
+            this.datePickerBirthday.Location = new System.Drawing.Point(121, 254);
+            this.datePickerBirthday.Name = "datePickerBirthday";
+            this.datePickerBirthday.Size = new System.Drawing.Size(200, 20);
+            this.datePickerBirthday.TabIndex = 20;
+            // 
+            // cbGender
+            // 
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Location = new System.Drawing.Point(465, 192);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(134, 21);
+            this.cbGender.TabIndex = 21;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Arial Rounded MT Bold", 21.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(36, 18);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(164, 33);
+            this.label10.TabIndex = 22;
+            this.label10.Text = "REGISTER";
+            // 
+            // frmRegistration
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 389);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.cbGender);
+            this.Controls.Add(this.datePickerBirthday);
+            this.Controls.Add(this.cbPrograms);
+            this.Controls.Add(this.txtContactNo);
+            this.Controls.Add(this.txtMiddleInitial);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.btnRegistration);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtStudentNo);
+            this.Controls.Add(this.label1);
+            this.Name = "frmRegistration";
+            this.Text = "Organization Profile";
+            this.Load += new System.EventHandler(this.frmRegistration_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStudentNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button btnRegistration;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtMiddleInitial;
+        private System.Windows.Forms.TextBox txtContactNo;
+        private System.Windows.Forms.ComboBox cbPrograms;
+        private System.Windows.Forms.DateTimePicker datePickerBirthday;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ HandsOnActivity2/Form1.cs
@@ -0,0 +1,257 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace HandsOnActivity2
+{
+
+
+    public partial class frmRegistration : Form
+    {
+
+        private String _FullName;
+        private int _Age;
+        private long _ContactNo;
+        private long _StudentNo;
+
+        public frmRegistration()
+        {
+            InitializeComponent();
+        }
+        public class StudentInformationClass
+        {
+
+            public static string SetFullName = "";
+            public static string SetBirthday = "";
+            public static string SetProgram = "";
+            public static string SetGender = "";
+            public static int SetStudentNo = 0;
+            public static int SetContactNo = 0;
+            public static int SetAge;
+
+        }
+
+
+
+        private void frmRegistration_Load(object sender, EventArgs e)
+        {
+            string[] ListOfProgram = new string[]{
+            
+            "BS Information Technology",
+            "BS Computer Science",
+            "BS Information Systems",
+            "BS in Accountancy",
+            "BS in Hospitality Management",
+            "BS in Tourism Management"
+            };
+            for (int i = 0; i < 6; i++)
+            {
+                cbPrograms.Items.Add(ListOfProgram[i].ToString());
+            }
+
+
+            string[] Genders = new string[]{
+                "Male", "Female"
+            };
+            for (int i = 0; i < 2; i++)
+            {
+                cbGender.Items.Add(Genders[i].ToString());
+            }
+        }
+     
+        public long StudentNumber(string studNum)
+        {
+            try
+            {
+                _StudentNo = long.Parse(studNum);
+
+            }
+            catch (FormatException e)
+            {
+                String error = "Student Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                String error = "Student Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (ArgumentNullException e)
+            {
+                String error = "Student Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (OverflowException e)
+            {
+                String error = "Student Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            finally
+            {
+
+                Console.WriteLine("Student Number is Executed");
+            }
+            
+                return _StudentNo;
+            }
+           
+    
+    
+        public long ContactNo(string Contact)
+        {
+            try
+            {
+                if (Regex.IsMatch(Contact, @"^[0-9]{10,11}$"))
+                {
+                    _ContactNo = long.Parse(Contact);
+
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Please Check the Contact Number Data");
+                }
+            }
+           
+            catch (FormatException e)
+            {
+                String error = "Contact Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                String error = "Contact Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (ArgumentException e)
+            {
+                String error = "Contact Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+         
+            catch (OverflowException e)
+            {
+                String error = "Contact Number Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            finally
+            {
+
+                Console.WriteLine("Contact Number is Executed");
+            }
+                    return _ContactNo;
+              
+        }
+
+        public string FullName(string LastName, string FirstName, string MiddleInitial)
+        {
+            try
+            {
+
+                if (Regex.IsMatch(LastName, @"^[a-zA-Z]+$") || Regex.IsMatch(FirstName, @"^[a-zA-Z]+$") || Regex.IsMatch(MiddleInitial, @"^[a-zA-Z]+$"))
+                {
+                    _FullName = LastName + ", " + FirstName + ", " + MiddleInitial;
+                }
+                else
+                {
+                    throw new ArgumentException("Please Check Name");
+                }
+            }
+            catch (FormatException e)
+            {
+                String error = "Name Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                String error = "Name Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (ArgumentException e)
+            {
+                String error = "Name Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+         
+            catch (OverflowException e)
+            {
+                String error = "Name Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            finally
+            {
+                Console.WriteLine("Name is Executed");
+            }
+            return _FullName;
+        }
+          
+
+        public int Age(string age)
+        {
+            try
+            {
+                if (Regex.IsMatch(age, @"^[0-9]{1,3}$"))
+                {
+                    _Age = Int32.Parse(age);
+                }
+                else
+                {
+                    throw new IndexOutOfRangeException("Please Check Age");
+                }
+            }
+
+            catch (FormatException e)
+            {
+                String error = "Age Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                String error = "Age Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (ArgumentNullException e)
+            {
+                String error = "Age Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            catch (OverflowException e)
+            {
+                String error = "Age Error: " + e.Message;
+                MessageBox.Show(error);
+            }
+            finally {
+
+                Console.WriteLine("Age is Executed");
+            }
+            return _Age;
+        }
+
+        private void btnRegistration_Click(object sender, EventArgs e)
+        {
+
+            StudentInformationClass.SetFullName = FullName(txtLastName.Text, txtFirstName.Text, txtMiddleInitial.Text);
+            StudentInformationClass.SetStudentNo = (int)StudentNumber(txtStudentNo.Text);
+            StudentInformationClass.SetProgram = cbPrograms.Text;
+            StudentInformationClass.SetGender = cbGender.Text;
+            StudentInformationClass.SetContactNo = (int)ContactNo(txtContactNo.Text);
+            StudentInformationClass.SetBirthday = datePickerBirthday.Value.ToString("yyyy-MM-dd");
+            StudentInformationClass.SetAge = Age(txtAge.Text);
+
+            frmConfirmation frm = new frmConfirmation();
+            frm.ShowDialog();
+
+          
+        }
+
+    }
+}
+        
+    
+
